05eefe9be2fbb77a8cafceab4788882ffe271478
micko2001 <micko.wijayanto57@gmail.com>
2024-12-18 14:29:09 +0700

    feat:db.json

--- db/db.js
+++ db/db.js
@@ -1,10 +1,13 @@
 const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "walled",
-  password: "micko147",
-  port: 5432,
-});
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "walled",
+//   password: "micko147",
+//   port: 5432,
+// });
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 module.exports = pool;
